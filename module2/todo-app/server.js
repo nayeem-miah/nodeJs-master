f07b0aae80e8d9833fa3018c1f7129218a0fae73
nayeem-miah <nayeem5113a@gmail.com>
2025-07-02 00:54:13 +0600

    http route

--- module2/todo-app/server.js
+++ module2/todo-app/server.js
@@ -1,8 +1,18 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
-    console.log({ req, res });
-    res.end("sever is connect......")
+    // console.log(req.url, req.method);
+    if (req.url === "/todo/update-todos" && req.method === "PATCH") {
+        // console.log("data update success");
+        res.end("data updated success")
+    } else if (req.url === "/todo/create-todos" && req.method === "POST") {
+        res.end("todo inserted success")
+    } else if (req.url === "/todo/getall" && req.method === "GET") {
+        res.end("get all todos success")
+    }
+    else {
+        res.end("route not found")
+    }
 })
 
 server.listen(5000, "127.0.0.1", () => {
