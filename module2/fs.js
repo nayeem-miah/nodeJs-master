1c60455c96481f854e7a440de0713ad05a9de7e4
nayeem-miah <nayeem5113a@gmail.com>
2025-07-01 13:08:31 +0600

    Stream and buffer

--- module2/fs.js
+++ module2/fs.js
@@ -7,26 +7,55 @@
 
 //  read file asynchronous
 const fs = require('fs');
-let text = "loading text......learning node js"
-console.log("task 1");
-const data = fs.readFile('./hello.txt', { encoding: "utf8" }, (error, data) => {
-    if (error) {
-        console.log("something went wrong !", error);
-    }
-    text = data;
+
+// fs.readFile('./helloWorld.txt', { encoding: "utf8" }, (error, data) => {
+//     if (error) {
+//         console.log("something went wrong !", error);
+//     };
+
+
+//     //  asynchronous write file
+//     fs.writeFile("./hello.txt", data, { encoding: "utf8" }, (err) => {
+//         if (err) {
+//             console.log("Something is error", err);
+//             return;
+//         }
+//         console.log('File written successfully!');
+
+//     })
+// }
+// );
+
+//  Stream and buffer
+const readStream = fs.createReadStream("./helloWorld.txt", { encoding: "utf-8" });
+const writeStream = fs.createWriteStream("./hello.txt", { encoding: "utf-8" });
+
+readStream.on("data", (data) => {
     console.log(data);
-}
-);
-console.log(text);
-console.log("task 3");
+    writeStream.write(data, (err) => {
+        if (err) {
+            throw Error("something is error", err)
+        }
+    })
+});
 
-//  asynchronous write file
-const text2 = "hello write file in node js";
-fs.writeFile("./hello.txt", text, { encoding: "utf8" }, (err) => {
+// error catch
+readStream.on("error", (err) => {
+    if (err) throw Error("something is error", err)
+})
+
+// write stream error catch
+writeStream.on("error", (err) => {
     if (err) {
-        console.log("Something is error", err);
-        return;
+        throw Error(err)
     }
-    console.log('File written successfully!');
+});
 
+readStream.on("end", () => {
+    console.log("reading ended");
+    writeStream.end();
+});
+
+writeStream.on("finish", () => {
+    console.log("write successfully");
 })
