cfaa3895a143a193f60b0786fed63c536da84cdb
nayeem-miah <nayeem5113a@gmail.com>
2025-07-02 08:41:06 +0600

    get dat in todo server

--- module2/todo-app/server.js
+++ module2/todo-app/server.js
@@ -1,18 +1,45 @@
 const http = require('node:http');
 
+const data = [
+    {
+        title: "learning node js ",
+        description: "hello node js . node js is a runtime language in javascript",
+        date: new Date().toLocaleDateString()
+    },
+    {
+        title: "learning express js ",
+        description: "hello express js . express js  is a node.js framework",
+        date: new Date().toLocaleDateString()
+    },
+    {
+        title: "learning node js ",
+        description: "hello node js . node js is a runtime language in javascript",
+        date: new Date().toLocaleDateString()
+    },
+]
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method);
     if (req.url === "/todo/update-todos" && req.method === "PATCH") {
         // console.log("data update success");
         res.end("data updated success")
+
     } else if (req.url === "/todo/create-todos" && req.method === "POST") {
         res.end("todo inserted success")
     } else if (req.url === "/todo/getall" && req.method === "GET") {
-        res.end("get all todos success")
+        res.writeHead(200, {
+            "content-type": "application/json",
+            // "email": "gh@gmail.com",
+        })
+        // res.setHeader("content-type", "text/plain");
+        // res.setHeader("gmail", "h4@gmail.com");
+        // res.statusCode = 201;
+        res.end(`<h2>hello html</h2>`);
+        // res.end(JSON.stringify(data));
     }
     else {
         res.end("route not found")
     }
+
 })
 
 server.listen(5000, "127.0.0.1", () => {
