c913990969e468aa1fb7507103411ae95d1831bc
nayeem-miah <nayeem5113a@gmail.com>
2025-07-03 16:10:22 +0600

    mongodb connected

--- module3/src/server.ts
+++ module3/src/server.ts
@@ -1,9 +1,34 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app"
 
 let server
-const port = 5000
+const port = 5000;
+// const uri = "mongodb+srv://todosApp:todosApp@cluster0.bomlehy.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = "mongodb://localhost:27017/todosDB"
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
 
 const bootstrap = async () => {
+    await client.connect();
+    console.log("connect to mongoDB");
+
+    // const db = await client.db("todosDB");
+    // // console.log(db);
+    // const collection = await db.collection("todos").insertOne({
+    //     title: "nodejs",
+    //     body: "nodejs is a js frameworks"
+    // })
+
+
     server = app.listen(port, () => {
         console.log(`Example app listening on port http://localhost:${port}`)
     })
