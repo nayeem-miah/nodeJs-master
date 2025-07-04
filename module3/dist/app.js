cd6704522f0bb28c38b4b64cca3ab0b0776d46b2
nayeem-miah <nayeem5113a@gmail.com>
2025-07-04 09:07:29 +0600

    route not found and global error handling

--- module3/dist/app.js
+++ module3/dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,6 +18,50 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json()); // JSON middleware
 // application routes
 app.use("/todos", todos_routes_1.default);
+app.get("/", (req, res, next) => {
+    // console.log({
+    //     url: req.url,
+    //     method: req.method,
+    //     header: req.header
+    // });
+    next();
+}, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send("welcome to todo app");
+    }
+    catch (error) {
+        res.status(500).json({
+            message: "something went wrong ",
+            error: error
+        });
+    }
+}));
+//  create custom error handler
+app.get("/error", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // console.log(something);
+        res.send("something went error");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+// route not found 
+app.use((req, res, next) => {
+    res.status(404).send({
+        message: "route not found"
+    });
+});
+// global error handling 
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log(error);
+        res.status(400).json({
+            message: "something went wrong .....",
+            error: error
+        });
+    }
+});
 exports.default = app;
 /**
  * server ---> server handling link --- starting, closing error handling of server. only related to server
--- module3/src/app.ts
+++ module3/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import todosRouter from "./app/todos/todos.routes";
 
 const app: Application = express();
@@ -8,6 +8,57 @@ app.use(express.json()); // JSON middleware
 // application routes
 app.use("/todos", todosRouter);
 
+
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    // console.log({
+    //     url: req.url,
+    //     method: req.method,
+    //     header: req.header
+    // });
+    next()
+},
+    async (req: Request, res: Response) => {
+        try {
+            res.send("welcome to todo app");
+        } catch (error) {
+            res.status(500).json({
+                message: "something went wrong ",
+                error: error
+            })
+        }
+    }
+)
+
+//  create custom error handler
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // console.log(something);
+        res.send("something went error")
+    } catch (error) {
+        next(error)
+    }
+
+}
+)
+
+// route not found 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send({
+        message: "route not found"
+    })
+})
+
+// global error handling 
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error) {
+        console.log(error);
+        res.status(400).json({
+            message: "something went wrong .....",
+            error: error
+        })
+    }
+})
 export default app;
 
 /**
