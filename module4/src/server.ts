63768fab6966585a8c65aae38edbce6571a5c076
nayeem-miah <nayeem5113a@gmail.com>
2025-07-08 08:42:04 +0600

    mongodb connect with mongooose

--- module4/src/server.ts
+++ module4/src/server.ts
@@ -1,1 +1,23 @@
-console.log("Server is running on port 3000");
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+
+
+let server: Server;
+const PORT = process.env.PORT || 5000;
+const uri: string = process.env.MONGO_URI || "mongodb://localhost:27017/"
+
+
+async function main() {
+    try {
+        await mongoose.connect(uri)
+        console.log("connected to mongodb using mongoose...");
+        server = app.listen(PORT, () => {
+            console.log(`app is listening prot http://localhost:${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+main()
